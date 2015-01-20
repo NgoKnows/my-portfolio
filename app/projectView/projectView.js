7940cdb8884d9619fdae7fb95268366945722196
Alex Ngo <alex_ngo@live.com>
2015-01-20 10:50:26 -0800

    created projectView controller w/ nav functions

--- app/projectView/projectView.js
+++ app/projectView/projectView.js
@@ -1,18 +1,35 @@
 "use strict"
-
-angular.module('myPortfolio', [
-    'ngRoute',
-    'ngPortfolio.homeView',
-    'ngPortfolio.projectView',
-    'ngPortfolio.aboutView'
-])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/homeView'});
+angular.module('myPortfolio.projectView', ['ngRoute'])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.when('/project', {
+                templateUrl: 'app/projectView/projectView.html',
+                controller: 'ProjectController'
+            });
 }])
-angular.element(document).ready(function(){
-    $('#hi').fadeIn(1200);
-});
-
-
-
-
+    .controller('ProjectController', function ($scope, $location) {
+        $scope.left = '/about';
+        $scope.right = '/';
+        $scope.slide = function (direction) {
+            if (direction === 'left' && $scope.left) {
+                console.log(direction);
+                $location.path($scope.left)
+            } else if (direction === 'right' && $scope.right) {
+                console.log(direction);
+                $location.path($scope.right);
+            }
+        }
+        $scope.keyPress = function (event) {
+            if (event.keyCode == 37) {
+                $scope.slide('left');
+            } else if (event.keyCode == 39) {
+                $scope.slide('right');
+            }
+        }
+        $scope.$on('$viewContentLoaded', function () {
+            console.log('did it work?');
+            setTimeout(function () {
+                $("#projectMain").focus();
+            }, 10);
+        });
+    });
