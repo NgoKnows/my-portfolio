778f972be8e0cc2a5c4c45b6d411d235a2df5601
Alex Ngo <alex_ngo@live.com>
2015-01-20 10:43:50 -0800

    about controller created w/ arrow navigation

--- app/aboutView/aboutView.js
+++ app/aboutView/aboutView.js
@@ -7,4 +7,28 @@ angular.module('myPortfolio.aboutView', ['ngRoute'])
                 controller: 'AboutController'
             });
 }])
-    .controller('AboutController', function ($scope) {});
+    .controller('AboutController', function ($scope, $location) {
+        $scope.left = '/';
+        $scope.right = '/projects';
+        $scope.slide = function (direction) {
+            if (direction === 'left' && $scope.left) {
+                console.log(direction);
+                $location.path($scope.left)
+            } else if (direction === 'right' && $scope.right) {
+                console.log(direction);
+                $location.path('/project');
+            }
+        }
+        $scope.keyPress = function (event) {
+            if (event.keyCode == 37) {
+                $scope.slide('left');
+            } else if (event.keyCode == 39) {
+                $scope.slide('right');
+            }
+        }
+                $scope.$on('$viewContentLoaded', function () {
+            setTimeout(function () {
+                $("#focusAbout").focus();
+            }, 1);
+        });
+    });
