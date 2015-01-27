710ad9673388d9038f0650439db1593a7ca2ed14
Alex Ngo <alex_ngo@live.com>
2015-01-26 23:18:48 -0800

    took out console.logs

--- app/projectView/project.js
+++ app/projectView/project.js
@@ -8,9 +8,6 @@ $.getJSON('../../data/projects.json')
             projectDict["link"] = project.link;
             projectDict["technology"] = project.technology;
             projectDict["picture"] = project.picture;
-            console.log(project.description);
-            console.log(project.link);
-            console.log(project.picture);
             projectList.push(projectDict);
         })
     });
