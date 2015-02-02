95641f5cfc61e2cf63a9f98e524035bdcee208f7
Alex Ngo <alex_ngo@live.com>
2015-02-01 22:28:13 -0800

    made a hover to show label feature

--- app/projectView/projectView.js
+++ app/projectView/projectView.js
@@ -1,10 +1,4 @@
 "use strict"
-//var projectnames = ["Everything Sports", "NBA Analysis and Web Crawler", "My Portfolio"]
-//var gitlink = ["https://github.com/NgoKnows/Everything-Sports","https://github.com/NgoKnows/NBA-Analysis", "https://github.com/NgoKnows/my-portfolio"]
-//var description = ["Hda", "djafsd", "dafjsfi"];
-//var a = {a: {name: "Everything Sports", description: "A application that provides users tools to help them succeed in fantasy basketball.", img: "..}, b: {name: "Everything Sports", description: "A application that provides users tools to help them succeed in fantasy basketball."}
-//}
-//var technologiesUsed = []
 angular.module('myPortfolio.projectView', ['ngRoute'])
     .config(['$routeProvider',
         function ($routeProvider) {
@@ -16,17 +10,22 @@ angular.module('myPortfolio.projectView', ['ngRoute'])
     .controller('ProjectController', function ($scope, $location) {
         //name, desc, link, technology
         $scope.projectList = projectList;
-        $scope.techImages = ["../../img/logos/android.svg", "../../img/logos/java.svg", "../../img/logos/python.svg", "../../img/logos/html.svg", "../../img/logos/javascript.svg", "../../img/logos/css.svg", "../../img/logos/angular.svg"]
+        $scope.techImages = ["../../img/logos/android.svg", "../../img/logos/java.svg", "../../img/logos/python.svg", "../../img/logos/html.svg", "../../img/logos/javascript.svg", "../../img/logos/css.svg", "../../img/logos/angular.svg", "../../img/logos/d3.svg"];
+        $scope.techLabels =
+["Android", "Java", "Python", "HTML5", "JavaScript", "CSS3", "AngularJS", "D3.js"];
         $scope.left = '/about';
         $scope.right = '/';
         $scope.getTech = function (index) {
             return $scope.techImages[index - 1];
         }
+        $scope.getLabel = function (index) {
+            return $scope.techLabels[index - 1];
+        }
         $scope.slide = function (direction) {
             if (direction === 'left' && $scope.left) {
                 $location.path($scope.left)
             } else if (direction === 'right' && $scope.right) {
-                $location.path($scope.right);
+                //$location.path($scope.right);
             }
         }
         $scope.keyPress = function (event) {
@@ -49,7 +48,18 @@ angular.module('myPortfolio.projectView', ['ngRoute'])
                 $(img).removeClass("hover");
             }
         }
+        $scope.showLabel = function (parentIndex, index, show) {
+            var label = "#label" + parentIndex + index;
+            console.log(label);
+            if (show) {
+                $(label).addClass("shown");
+
+            } else {
+                $(label).removeClass("shown");
+            }
+        }
         $scope.$on('$viewContentLoaded', function () {
+            console.log('linked up');
             setTimeout(function () {
                 $("#projectMain").focus();
                 $(this).scrollTop(0);
