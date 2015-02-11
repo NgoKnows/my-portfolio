2bac78d623de408b38f92afe6dc33b315fe3d61e
Alex Ngo <alex_ngo@live.com>
2015-02-10 23:34:24 -0800

    added 4th project

--- data/makeJSON.py
+++ data/makeJSON.py
@@ -2,18 +2,24 @@ __author__ = 'Alex'
 
 import json
 
-project_names = ["Everything Sports", "NBA Analysis and Web Crawler", "My Portfolio"]
+project_names = ["Everything Sports",
+                 "Zillow HackHousing Hackathon | Hood Scout",
+                 "NBA Analysis and Web Crawler",
+                 "My Portfolio"]
 project_descriptions = ["Fantasy Basketball tools to help users succeed!",
+                        "Helping people find their perfect neighborhood",
                         "NBA Statistics web crawler and analysis of statistics",
                         "My personal website, you're on it!"]
 github_links = ["https://github.com/NgoKnows/Everything-Sports",
+                'https://github.com/ZillowHackathonTeamHeat/HeatMap',
                 "https://github.com/NgoKnows/NBA-Analysis",
                 "https://github.com/NgoKnows/my-portfolio"]
 
 
-#1: android, 2: java, 3: python, 4: html, 5: js, 6: css, 7: angular
-technologies = [[3, 4, 5, 6, 7],[3],[3, 4, 5, 6, 7]]
+#1: android, 2: java, 3: python, 4: html, 5: js, 6: css, 7: angular, 8: googlemaps
+technologies = [[3, 4, 5, 6, 7, 8], [4, 5, 6, 9], [3],[3, 4, 5, 6, 7]]
 pictureURLS = ["../../img/projects/fantasy.png",
+               "../../img/projects/2015-02-10 20_43_54-Heatmaps.png",
                "../../img/projects/scatter.png",
                "../../img/projects/portfolio.png"]
 project_list = []
