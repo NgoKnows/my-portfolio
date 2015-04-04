f8215c77a9ee980b62f4880ec900fa3574648900
Alex Ngo <alex_ngo@live.com>
2015-04-04 14:51:36 -0700

    added showStaggeredList and show for skill list

--- app/aboutView/aboutView.js
+++ app/aboutView/aboutView.js
@@ -7,10 +7,20 @@ angular.module('myPortfolio.aboutView', ['ngRoute'])
                 controller: 'AboutController'
             });
 }])
-    .controller('AboutController', function ($scope, $location) {
+    .controller('AboutController', function ($scope) {
         $scope.$on('$viewContentLoaded', function () {
             setTimeout(function () {
                 $(this).scrollTop(0);
-            }, 5);
+            }, 10);
+//            $(window).resize(function () {
+//                if(
+//            });
+            setTimeout(function () {
+                Materialize.showStaggeredList('#skills');
+                setTimeout(function () {
+                    $('#skills').show(1500);
+                }, 1000);
+
+            }, 1500);
         });
     });
